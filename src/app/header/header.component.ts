04d573ff7bee5f471b35facf301786337ad1729c
JhonHeSTR <jhon.arcila@stratesys-ts.com>
2025-09-21 16:31:15 -0500

    update day param in url

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { ThemeService } from '../theme.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,26 +10,40 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private service: SharedService, public themeService: ThemeService, public router: Router) { }
+  constructor(private service: SharedService, public themeService: ThemeService, public router: Router, public activatedRoute: ActivatedRoute) { }
 
   today?: any;
   data?: any;
   days?: any;
-  current?: string;
+  current: string = '';
 
   ngOnInit(): void {
+
     this.service.data.subscribe(data => {
+
       this.data = data;
+
       if (!this.data) return;
-      this.today = this.data.days.filter((d: any) => d.code === this.data.code)[0];
-      this.current = this.today.day;
+
       this.days = [...this.data.days];
       this.days.sort((a: any, b: any) => {
         if (a.day === 'Domingo') return 1;
         if (b.day === 'Domingo') return -1;
         return 0;
       });
-      this.router.navigate([this.current]);
+
+      const param = this.activatedRoute.snapshot.paramMap.get('day');
+
+      if (param) {
+        const param_day = this.data.days.filter((d: any) => this.removeAccent(d.day) === param)[0];
+        this.router.navigate([param_day.day]);
+        this.today = param_day;
+        return;
+      }
+
+      this.today = this.data.days.filter((d: any) => d.code === this.data.code)[0];
+      this.current = this.today.day;
+      this.router.navigate([this.removeAccent(this.current)]);
     });
   }
 
@@ -40,6 +54,10 @@ export class HeaderComponent implements OnInit {
   changeDay(day: string): void {
     this.today = this.data.days.filter((d: any) => d.code === day)[0];
     this.service.setDay(day);
-    this.router.navigate([this.today.day.normalize("NFD").replace(/[\u0300-\u036f]/g, "")]);
+    this.router.navigate([this.removeAccent(this.today.day)]);
+  }
+
+  removeAccent(word: string): string {
+    return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   }
 }
